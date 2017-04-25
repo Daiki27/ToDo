c7c09ef39be7b663ad3b73877479c1ff338391fb
樋口大樹 <daikiit27@gmail.com>
2017-04-25 20:48:02 +0900

    Initial Commit

---
+++ original apri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  original apri
+//
+//  Created by 樋口大樹 on 2017/04/25.
+//  Copyright © 2017年 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
