63a5064c53012b7bebfe9e5959e17362a980ed33
樋口大樹 <daikiit27@gmail.com>
2017-04-29 03:10:02 +0900

    画面遷移

---
+++ original apri/AddViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddViewController.swift
+//  original apri
+//
+//  Created by 樋口大樹 on 2017/04/29.
+//  Copyright © 2017年 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func modoru(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
