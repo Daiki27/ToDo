8bf52801e69760b49ecd46a58072905d4783320c
樋口大樹 <daikiit27@gmail.com>
2017-05-03 22:00:54 +0900

    aa

---
+++ original apri/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  original apri
+//
+//  Created by 樋口大樹 on 2017/05/03.
+//  Copyright © 2017年 樋口大樹. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
